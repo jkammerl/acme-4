9f84fab8ef18cc20145c4fdddd8ee475759c4e20
Julius Kammerl <julius@kammerl.de>
2020-03-07 23:38:20 +0100

    Rebuilt plugin against JUCE v5.4.7

--- Source/ACME4Editor.h
+++ Source/ACME4Editor.h
@@ -1,30 +1,26 @@
 /*
   ==============================================================================
 
-  This is an automatically generated file created by the Jucer!
-
-  Creation date:  23 Feb 2012 12:52:59am
+  This is an automatically generated GUI class created by the Projucer!
 
   Be careful when adding custom code to these files, as only the code within
   the "//[xyz]" and "//[/xyz]" sections will be retained when the file is loaded
   and re-saved.
 
-  Jucer version: 1.12
+  Created with Projucer version: 5.4.7
 
   ------------------------------------------------------------------------------
 
-  The Jucer is part of the JUCE library - "Jules' Utility Class Extensions"
-  Copyright 2004-6 by Raw Material Software ltd.
+  The Projucer is part of the JUCE library.
+  Copyright (c) 2017 - ROLI Ltd.
 
   ==============================================================================
 */
 
-#ifndef __JUCER_HEADER_ACMEPLUGINAUDIOPROCESSOREDITOR_ACMEEDITOR_CEC5CA03__
-#define __JUCER_HEADER_ACMEPLUGINAUDIOPROCESSOREDITOR_ACMEEDITOR_CEC5CA03__
+#pragma once
 
 //[Headers]     -- You can add your own extra header files here --
-#include "../JuceLibraryCode/JuceHeader.h"
-#include "../JuceLibraryCode/JucePluginCharacteristics.h"
+#include <JuceHeader.h>
 #include "PluginProcessor.h"
 //[/Headers]
 
@@ -33,59 +29,51 @@
 //==============================================================================
 /**
                                                                     //[Comments]
-    An auto-generated component, created by the Jucer.
+    An auto-generated component, created by the Projucer.
 
     Describe your class and how it works here!
                                                                     //[/Comments]
 */
-class ACME4PluginAudioProcessorEditor  : public AudioProcessorEditor,
-                                         public Timer
+class ACME4Editor  : public AudioProcessorEditor,
+                     public Timer
 {
 public:
     //==============================================================================
-    ACME4PluginAudioProcessorEditor (ACME4PluginAudioProcessor* ownerFilter);
-    ~ACME4PluginAudioProcessorEditor();
+    ACME4Editor (Acme4v2AudioProcessor& processor);
+    ~ACME4Editor() override;
 
     //==============================================================================
     //[UserMethods]     -- You can add your own custom methods in this section.
-	void timerCallback();
+    void timerCallback() override;
     //[/UserMethods]
 
-    void paint (Graphics& g);
-    void resized();
+    void paint (Graphics& g) override;
+    void resized() override;
 
     // Binary resources:
     static const char* acme4_scaled_png;
     static const int acme4_scaled_pngSize;
 
 
-    //==============================================================================
-    juce_UseDebuggingNewOperator
-
 private:
     //[UserVariables]   -- You can add your own custom variables in this section.
-
     AudioPlayHead::CurrentPositionInfo lastDisplayedPosition;
 
-    ACME4PluginAudioProcessor* getProcessor() const
-    {
-        return static_cast <ACME4PluginAudioProcessor*> (getAudioProcessor());
-    }
+    Acme4v2AudioProcessor& processor_;
 
     void displayPositionInfo (const AudioPlayHead::CurrentPositionInfo& pos);
     //[/UserVariables]
 
     //==============================================================================
-    Label* infoLabel;
-    Label* label;
-    Image cachedImage_acme4_scaled_png;
+    std::unique_ptr<Label> infoLabel;
+    std::unique_ptr<Label> label;
+    Image cachedImage_acme4_scaled_png_1;
 
 
     //==============================================================================
-    // (prevent copy constructor and operator= being generated..)
-    ACME4PluginAudioProcessorEditor (const ACME4PluginAudioProcessorEditor&);
-    const ACME4PluginAudioProcessorEditor& operator= (const ACME4PluginAudioProcessorEditor&);
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (ACME4Editor)
 };
 
+//[EndFile] You can add extra defines here...
+//[/EndFile]
 
-#endif   // __JUCER_HEADER_ACMEPLUGINAUDIOPROCESSOREDITOR_ACMEEDITOR_CEC5CA03__
